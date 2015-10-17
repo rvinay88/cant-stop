7d5153aac754f93f7538a0a9fda19c0c2acd25f6
Vinay Raghu <rvinay88@gmail.com>
2015-10-17 10:18:22 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  cant-stop
+//
+//  Created by Vinay  Raghu on 10/17/15.
+//
+//
+
+#include "tools.hpp"
+
+int main() {
+    banner();
+    puts( "C++ programs" );
+    bye();
+    return 0;
+}
